765076cf6d6a369af75cd633740578305d821ff0
Adrian Gushin <agushin@Adrians-MacBook-Pro.local>
2022-10-15 17:07:10 -0400

    Added framework for database

--- src/ProfileGallery/ProfileForm/ProfileUpload/ProfileUploadAll.js
+++ src/ProfileGallery/ProfileForm/ProfileUpload/ProfileUploadAll.js
@@ -1,20 +1,38 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
-const Create = ({page, setPage}) => {
-    return(
-        <section className="profile_grad_all profile_section_all">
+const Create = ({ page, setPage }) => {
+    return (
+
+        <Routes>
+            <Route path='/' element={<Default> </Default>}>
+            </Route>
+            <Route path='/submit' element={<Submit ></Submit>}> </Route>
+        </Routes>
+
+    );
+
+}
+
+function Default({page, setPage}) {
+    const navigate = useNavigate();
+    return (
+        <section className="profile_grad_all profile_section_all" >
             <h4>Upload Statement of Purpose & CV</h4>
             <p>Please erase all your personal information</p>
-            <button>Upload SOP</button><br/>
-            <button>CV</button><br/>
-            <input type="checkbox"/>
-            <label for="vehicle1">I have hidden all the information that I believe is sensitive</label><br></br>
-            
-            <button onClick={() => {setPage(page - 1);}}>Previous</button><br/>
+            <button>Upload SOP</button><br />
+            <button>CV</button><br />
+            <input type="checkbox" />
+            <label htmlFor="vehicle1">I have hidden all the information that I believe is sensitive</label><br></br>
 
-            <button>Submit</button>
-        </section>
-    ); 
+            <button onClick={() => { setPage(page - 1); }}>Previous</button><br />
 
+            <button onClick={() => { navigate('/submit'); }}>Submit</button>
+        </section >
+    );
+
+}
+function Submit() {
+    return <h2>Thanks for the submission!</h2>;
 }
 
 export default Create;
