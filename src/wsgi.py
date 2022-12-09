49f2ea0c575c6640c6f879d2e75ebfed596c3f62
TiantianLi28 <102604240+TiantianLi28@users.noreply.github.com>
2022-12-09 18:47:25 -0500

    Delete src directory

--- src/wsgi.py
+++
@@ -1,8 +0,0 @@
-##The entry script for the Flask server. This script starts the server.
-
-from backend import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
